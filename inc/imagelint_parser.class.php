af185e5954415b27925b8e3e9a4dfec9a9fad02a
René Roth <mail@reneroth.org>
2017-03-06 15:32:37 +0100

    Parse style tags

--- inc/imagelint_parser.class.php
+++ inc/imagelint_parser.class.php
@@ -3,6 +3,8 @@ use voku\helper\HtmlDomParser;
 
 class ImageLint_Parser {
 
+  const CSSURLREGEX = "|url\\(['\"]*(.*?)['\"]*\\)|i";
+
   static function parseHook() {
     ob_start(__CLASS__ . '::parseHandler');
   }
@@ -15,7 +17,16 @@ class ImageLint_Parser {
       $element->src = self::getUrl($element->src);
     }
 
+    foreach($html->find('*[style]') as $element) {
+      $output = $element->style;
+      $element->style = preg_replace_callback(self::CSSURLREGEX, __CLASS__ . '::parseCSSUrl', $element->style);
+    }
+
     return $html;
+  }
+
+  static function parseCSSUrl($matches) {
+    return 'url("' . self::getUrl($matches[1]) . '")';
   }
 
   // Placeholder Method, this will be replaced by an external library
